d224d2f8905c311f9236c7a5b07db210c546db7d
Manpreet <m81092@gmail.com>
2021-03-14 21:02:57 -0400

    'multi-module'

---
+++ pet-clinic-data/src/main/java/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package petclinic.model;
+
+public class Vet extends Person {
+
+}
--- pet-clinic-web/src/main/java/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/petclinic/PetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.udemy.petclinic;
+package petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
